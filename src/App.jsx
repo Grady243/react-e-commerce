3cc1e0753942bf2d505ecf02153cfda6d837d05b
Grady243 <gradyaganzemasirika@gmail.com>
2026-02-07 14:41:36 +0100

    fix: resolve final initialization errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FaReact } from "react-icons/fa";
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold text-blue-500">
       Tailwind fonctionne 🎉
     </h1>
+    <FaReact/>
+    </>
   )
 }
 
