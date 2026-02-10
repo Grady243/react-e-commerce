d052c7df197fe9ed5738356c6a1e545a6237fb1e
Grady243 <gradyaganzemasirika@gmail.com>
2026-02-10 13:23:18 +0100

    feat: create menu pages structure

---
+++ src/Pages/Collection.jsx
@@ -0,0 +1,10 @@
+function Collection() {
+  return (
+    <div>
+      <h1>Categories Page</h1>
+      <p>This is the collection page.</p>
+    </div>
+  );
+}
+
+export default Collection;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,10 @@
+function Shop() {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+      <p>This is the shop page.</p>
+    </div>
+  );
+}
+
+export default Shop;
