2b0851d9a541a42e0872a1cffb3635c03ce22c83
Grady243 <gradyaganzemasirika@gmail.com>
2026-02-12 07:07:10 +0100

    feat : implente shop page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Collection from "./pages/Collection";
-import Blog from "./pages/Blog";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import Collection from "./Pages/Collection";
+import Blog from "./Pages/Blog";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
